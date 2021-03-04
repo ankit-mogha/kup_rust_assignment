d3e60f6f82a5ffe9004fa6b38f0351179a18f87f
Ankit Mogha <ankit.mogha@knoldus.com>
2021-03-04 11:56:57 +0530

    Assignment-5

---
+++ Assignments/assignment_five/src/lib.rs
@@ -0,0 +1,6 @@
+#[allow(dead_code)]
+mod ques1 ;
+#[allow(dead_code)]
+mod ques2;
+mod test;
+
--- Assignments/workflow-check/src/lib.rs
+++ Assignments/workflow-check/src/lib.rs
@@ -2,13 +2,13 @@
 mod tests {
     use crate::testWorkflow;
     #[test]
-    assert_eq!(testWorkflow(1,2),5);
+    assert_eq!(testWorkflow(1, 2), 5);
 }
 
-fn testWorkflow (a: i32, b: i32) -> i32 {
+fn testWorkflow(a: i32, b: i32) -> i32 {
     a + b
 }
 
-fn testWorkflow1 (a: i32, b: i32) -> i32 {
+fn testWorkflow1(a: i32, b: i32) -> i32 {
     a + b
 }
